4e55077705c2f45a819a89306c2a85081ba80942
David Hellmers <david@appmotion.de>
2023-08-21 09:54:33 +0200

    feat: use compose bom

--- ShoppingListAndroid/build.gradle.kts
+++ ShoppingListAndroid/build.gradle.kts
@@ -45,11 +45,17 @@ detekt {
 
 dependencies {
     implementation(project(":ShoppingListCore"))
-    implementation("androidx.compose.ui:ui:1.5.0")
-    implementation("androidx.compose.ui:ui-tooling:1.5.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.0")
-    implementation("androidx.compose.foundation:foundation:1.5.0")
-    implementation("androidx.compose.material:material:1.5.0")
+
+    // Compose Bill of Materials
+    val composeBom = platform("androidx.compose:compose-bom:2023.08.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.material:material")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.navigation:navigation-compose:2.7.0")
 }
