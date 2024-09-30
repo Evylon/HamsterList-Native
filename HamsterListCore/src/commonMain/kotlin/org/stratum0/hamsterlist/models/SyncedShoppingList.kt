488072a026c3eee30981cfc9aadd9c2fb2f5ec90
David Hellmers <mail@evylon.de>
2024-09-30 23:21:37 +0200

    always serialise empty list to avoid 400 from server

--- HamsterListCore/src/commonMain/kotlin/org/stratum0/hamsterlist/models/SyncedShoppingList.kt
+++ HamsterListCore/src/commonMain/kotlin/org/stratum0/hamsterlist/models/SyncedShoppingList.kt
@@ -1,13 +1,17 @@
 package org.stratum0.hamsterlist.models
 
+import kotlinx.serialization.EncodeDefault
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 
+@OptIn(ExperimentalSerializationApi::class)
 @Serializable
 data class SyncedShoppingList(
     val id: String = "",
     val title: String = "",
     val token: String = "",
     val changeId: String? = null,
+    @EncodeDefault
     var items: List<Item> = emptyList()
 ) {
     fun toShoppingList(): ShoppingList = ShoppingList(
