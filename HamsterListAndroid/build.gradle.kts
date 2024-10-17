6a122349cbf54f81a8cd9bebd0d0db06c8b1c864
David Hellmers <mail@evylon.de>
2024-10-17 13:39:35 -0300

    increment versionCode for v1.1-RC3

--- HamsterListAndroid/build.gradle.kts
+++ HamsterListAndroid/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "org.stratum0.hamsterlist.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
+        versionCode = 4
         versionName = "1.1+$buildTag"
     }
     signingConfigs {
