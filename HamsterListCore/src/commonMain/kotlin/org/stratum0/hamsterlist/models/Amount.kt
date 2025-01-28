559e80701a11dedd4b20dc752f429aa38b6d9aaf
David Hellmers <mail@evylon.de>
2025-01-27 22:20:24 -0300

    improve unit parsing and add some metric prefixes

--- HamsterListCore/src/commonMain/kotlin/org/stratum0/hamsterlist/models/Amount.kt
+++ HamsterListCore/src/commonMain/kotlin/org/stratum0/hamsterlist/models/Amount.kt
@@ -10,6 +10,8 @@ data class Amount(
     override fun toString() = "$value${unit ?: ""}"
 
     companion object {
+        private const val PATTERN_COMBINED = "^\\d+(\\.\\d+)?[a-zA-Z]+\$"
+
         fun parse(components: List<String>): Amount? {
             return when (components.size) {
                 1 -> {
@@ -19,7 +21,7 @@ data class Amount(
                 }
                 2 -> {
                     components[0].toDoubleOrNull()?.let { value ->
-                        components[1].takeIf { SIUnit.isSIUnit(it) }?.let { unit ->
+                        parseUnitAndPrefix(components[1])?.let { unit ->
                             Amount(value = value, unit = unit)
                         }
                     }
@@ -27,30 +29,56 @@ data class Amount(
                 else -> null
             }
         }
+
         private fun parseCombinedAmount(input: String): Amount? {
             if (!Regex(PATTERN_COMBINED).matches(input)) {
                 return null
             }
             val lastDigitIndex = input.indexOfLast { it.isDigit() }
             val value = input.substring(0, lastDigitIndex + 1).toDoubleOrNull()
-            val unit = input.substring(lastDigitIndex + 1)
-            return if (value != null && SIUnit.isSIUnit(unit)) {
+            val unit = parseUnitAndPrefix(input.substring(lastDigitIndex + 1))
+            return if (value != null && unit != null) {
                 Amount(value, unit)
             } else {
                 null
             }
         }
-        private const val PATTERN_COMBINED = "^\\d+(\\.\\d+)?[a-zA-Z]+\$"
+
+        private fun parseUnitAndPrefix(input: String): String? {
+            val onlyUnit = Unit.parse(input)
+            val unitWithOutPrefix = Unit.parse(input.takeLast(1))
+            val metricPrefix = MetricPrefix.parse(input.take(input.length - 1))
+            return if (onlyUnit != null) {
+                onlyUnit.symbol
+            } else if (unitWithOutPrefix != null && metricPrefix != null) {
+                metricPrefix.symbol + unitWithOutPrefix.symbol
+            } else {
+                null
+            }
+        }
     }
 }
 
-enum class SIUnit {
-    kg,
-    L,
-    m;
+enum class Unit(val symbol: String) {
+    GRAMM("g"),
+    LITRE("L"),
+    LITRE_LOWERCASE("l"),
+    METER("m");
+
     companion object {
-        // TODO add library for SI Unit parsing
-        fun isSIUnit(input: String): Boolean =
-            entries.any { input == it.name }
+        fun parse(input: String) = entries.firstOrNull { it.symbol == input }
+    }
+}
+
+enum class MetricPrefix(val symbol: String) {
+    KILO("k"),
+    HECTO("h"),
+    DECA("da"),
+    DECI("d"),
+    CENTI("c"),
+    MILLI("m");
+
+    companion object {
+        fun parse(input: String) = MetricPrefix.entries.firstOrNull { it.symbol == input }
     }
 }
