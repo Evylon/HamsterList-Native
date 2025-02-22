a6e68b304cefa52bf78f8afc4b15b1186fb98fee
David Hellmers <mail@evylon.de>
2025-02-22 14:02:33 -0300

    increment version for release v1.1-RC9

--- HamsterListAndroid/build.gradle.kts
+++ HamsterListAndroid/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "org.stratum0.hamsterlist.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
+        versionCode = 9
         versionName = "1.1+$versionCode"
     }
     signingConfigs {
