cf302f0e237581b3e6e00ef0765f3bfa9b743ab5
David Hellmers <david.hellmers@iserv.eu>
2025-11-01 18:51:59 +0100

    fix magic numbers

--- HamsterListAndroid/src/main/java/org/stratum0/hamsterlist/android/gui/components/HamsterListLoadingIndicator.kt
+++ HamsterListAndroid/src/main/java/org/stratum0/hamsterlist/android/gui/components/HamsterListLoadingIndicator.kt
@@ -1,6 +1,8 @@
 package org.stratum0.hamsterlist.android.gui.components
 
+import androidx.compose.animation.core.CubicBezierEasing
 import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.InfiniteRepeatableSpec
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
@@ -22,6 +24,16 @@ import androidx.compose.ui.unit.dp
 import org.stratum0.hamsterlist.android.HamsterListTheme
 import org.stratum0.hamsterlist.android.R
 
+private const val ANIMATION_DURATION = 500
+private const val BOUNCE_INITIAL = 0f
+private const val BOUNCE_FULL = 200f
+private const val STRETCH_X_INITIAL = 0.4f
+private const val STRETCH_X_FULL = 0.45f
+private const val STRETCH_Y_INITIAL = 0.4f
+private const val STRETCH_Y_FULL = 0.35f
+@Suppress("MagicNumber")
+private val easing = CubicBezierEasing(0.5f, 0.0f, 0.75f, 0.4f)
+
 @Composable
 fun HamsterListLoadingIndicator(
     modifier: Modifier = Modifier
@@ -29,26 +41,26 @@ fun HamsterListLoadingIndicator(
     val infiniteSpec = rememberInfiniteTransition()
     val repeatSpec: InfiniteRepeatableSpec<Float> = infiniteRepeatable(
         animation = tween(
-            500,
-            easing = FastOutLinearInEasing
+            durationMillis = ANIMATION_DURATION,
+            easing = easing
         ),
         repeatMode = RepeatMode.Reverse
     )
     val bounce = infiniteSpec.animateFloat(
-        0f,
-        200f,
+        initialValue = BOUNCE_INITIAL,
+        targetValue = BOUNCE_FULL,
         animationSpec = repeatSpec,
         label = "Loading Bounce"
     )
     val stretchX = infiniteSpec.animateFloat(
-        0.4f,
-        0.45f,
+        initialValue = STRETCH_X_INITIAL,
+        targetValue = STRETCH_X_FULL,
         animationSpec = repeatSpec,
         label = "Loading StretchX"
     )
     val stretchY = infiniteSpec.animateFloat(
-        0.4f,
-        0.35f,
+        initialValue = STRETCH_Y_INITIAL,
+        targetValue = STRETCH_Y_FULL,
         animationSpec = repeatSpec,
         label = "Loading StretchY"
     )
