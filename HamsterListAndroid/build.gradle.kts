02622701c4eda21ef0d979413176beddf143ecf4
David Hellmers <david.hellmers@iserv.eu>
2025-11-29 23:03:54 +0100

    bump version

--- HamsterListAndroid/build.gradle.kts
+++ HamsterListAndroid/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "org.stratum0.hamsterlist.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
+        versionCode = 22
         versionName = "1.1+$versionCode"
     }
     signingConfigs {
